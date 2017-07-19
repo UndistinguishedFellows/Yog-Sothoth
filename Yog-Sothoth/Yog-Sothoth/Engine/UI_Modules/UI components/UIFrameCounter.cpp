65e4fd405c2a4ed80d15b96453eb8b7892a97757
CapitanLiteral <anaraguen@gmail.com>
2017-07-19 09:13:06 +0200

    FPS counter

--- Yog-Sothoth/Yog-Sothoth/Engine/UI_Modules/UI components/UIFrameCounter.cpp
+++ Yog-Sothoth/Yog-Sothoth/Engine/UI_Modules/UI components/UIFrameCounter.cpp
@@ -20,14 +20,14 @@ void UIFrameCounter::DoUpdate(float dt)
 void UIFrameCounter::Draw()
 {
 	ImGui::SetNextWindowPos(ImVec2(10, 10));
-	if (!ImGui::Begin("Example: Fixed Overlay", &pOpen, ImVec2(0, 0), 0.3f, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings))
+	if (!ImGui::Begin("", &pOpen, ImVec2(0, 0), 0.3f, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings))
 	{
 		ImGui::End();
 		return;
 	}
-	ImGui::Text("Simple overlay\non the top-left side of the screen.");
-	ImGui::Separator();
-	ImGui::Text("Mouse Position: (%.3f,%.0f)", dt, fps);
+	ImGui::Text("MS: %.3f", dt);
+	//ImGui::Separator();
+	ImGui::Text("FPS: %.0f", fps);
 	ImGui::End();
 
 }
