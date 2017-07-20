fbed93bd098ff8df01969fd0f28cdf2e3fd786c5
CapitanLiteral <anaraguen@gmail.com>
2017-07-20 22:23:58 +0200

    Adding and removing child methods

--- Yog-Sothoth/Yog-Sothoth/Engine/GameObjects/GameObject.cpp
+++ Yog-Sothoth/Yog-Sothoth/Engine/GameObjects/GameObject.cpp
@@ -14,3 +14,44 @@ GameObject::~GameObject()
 	RemoveChildren();
 }
 
+void GameObject::AddChild(GameObject* child)
+{
+	children.push_back(child);
+}
+
+void GameObject::RemoveChild(GameObject* child)
+{
+	for (std::vector<GameObject*>::iterator it = children.begin(); 
+		 it != children.end(); ++it)
+	{
+		if ((*it) == child)
+		{
+			delete (*it);
+			children.erase(it);
+		}
+	}
+}
+void GameObject::EraseChild(GameObject* child)
+{
+	for (std::vector<GameObject*>::iterator it = children.begin();
+		 it != children.end(); ++it)
+	{
+		if ((*it) == child)
+		{
+			children.erase(it);
+		}
+	}
+
+}
+
+void GameObject::RemoveChildren()
+{
+	for (std::vector<GameObject*>::iterator it = children.begin();
+		 it != children.end(); ++it)
+	{
+		(*it)->RemoveChildren();
+		delete (*it);
+		children.erase(it);
+	}
+
+}
--- Yog-Sothoth/Yog-Sothoth/Engine/GameObjects/GameObject.h
+++ Yog-Sothoth/Yog-Sothoth/Engine/GameObjects/GameObject.h
@@ -14,4 +14,25 @@ public:
 	GameObject(GameObject* parent);
 	~GameObject();
 
+	/**
+	 * \brief Add a child into children list of this object
+	 * \param child Child to be added
+	 */
+	void AddChild(GameObject* child);
+	/**
+	 * \brief Will delete the child too
+	 * \param child Child to be Removed
+	 */
+	void RemoveChild(GameObject* child);
+	/**
+	 * \brief Will NOT delete the child
+	 * \param child Child to be erased
+	 */
+	void EraseChild(GameObject* child);  
+	/**
+	 * \brief Will delete all the children and their children recursively
+	 */
+	void RemoveChildren(); //Recursive
+
+
 };
