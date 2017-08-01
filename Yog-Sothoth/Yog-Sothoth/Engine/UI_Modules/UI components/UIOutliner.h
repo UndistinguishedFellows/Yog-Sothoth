1cae56db581f5ed69d10f5d4ee2e0d440d4bcd88
CapitanLiteral <anaraguen@gmail.com>
2017-08-02 00:33:30 +0200

    Outliner

---
+++ Yog-Sothoth/Yog-Sothoth/Engine/UI_Modules/UI components/UIOutliner.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "../../../imGUI/imgui.h"
+
+class UIOutliner
+{
+public:
+	UIOutliner();
+	virtual ~UIOutliner();
+
+	void draw();
+	void treeNodes(GameObject* node);
+
+	ImGuiWindowFlags outilnerWindowFlags = 0;
+
+};
