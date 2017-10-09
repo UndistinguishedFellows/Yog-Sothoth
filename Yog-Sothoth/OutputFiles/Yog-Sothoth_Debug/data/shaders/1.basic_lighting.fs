d0143009f638c808200cbec12b34656d82c497d6
CapitanLiteral <anaraguen@gmail.com>
2017-10-09 22:34:46 +0200

    Shader with texture

--- Yog-Sothoth/OutputFiles/Yog-Sothoth_Debug/data/shaders/1.basic_lighting.fs
+++ Yog-Sothoth/OutputFiles/Yog-Sothoth_Debug/data/shaders/1.basic_lighting.fs
@@ -3,31 +3,33 @@ out vec4 FragColor;
 
 in vec3 Normal;  
 in vec3 FragPos;  
+in vec2 TexCoords;
   
 uniform vec3 lightPos; 
 uniform vec3 viewPos; 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
+uniform sampler2D tex;
 
 void main()
 {
     // ambient
     float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-  	
+	vec3 ambient = ambientStrength * lightColor * texture(tex, TexCoords).rgb;  	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    
+	vec3 diffuse = diff * lightColor* texture(tex, TexCoords).rgb;    
     // specular
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
+    vec3 specular = specularStrength * spec * lightColor * texture(tex, TexCoords).rgb;   
         
     vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
+    //FragColor = texture(tex, TexCoords);
+    //FragColor = vec4(TexCoords.x, TexCoords.y, 0, 1);
 }
