b63ef6cd14c48967ac641f6a6962b632f437cf64
CapitanLiteral <anaraguen@gmail.com>
2017-11-17 10:21:32 +0100

    C_Light Deserialize

--- Yog-Sothoth/Yog-Sothoth/Engine/GameObjects/Components/C_Light.cpp
+++ Yog-Sothoth/Yog-Sothoth/Engine/GameObjects/Components/C_Light.cpp
@@ -10,17 +10,37 @@ void C_Light::Serialize(Json::Value& root)
 	root["ambient"].append(ambient.y);
 	root["ambient"].append(ambient.z);
 
-	root["diffuse"].append(ambient.x);
-	root["diffuse"].append(ambient.y);
-	root["diffuse"].append(ambient.z);
+	root["diffuse"].append(diffuse.x);
+	root["diffuse"].append(diffuse.y);
+	root["diffuse"].append(diffuse.z);
 
-	root["specular"].append(ambient.x);
-	root["specular"].append(ambient.y);
-	root["specular"].append(ambient.z);
+	root["specular"].append(specular.x);
+	root["specular"].append(specular.y);
+	root["specular"].append(specular.z);
 
 	root["type"] = type;
 }
 
 void C_Light::Deserialize(Json::Value& root)
 {
+	type = (ComponentType)root.get("type", 0).asInt();
+
+	Json::Value col = root["color"];
+	Json::Value::iterator it = col.begin();
+	specular.x = (*it++).asFloat();
+	specular.y = (*it++).asFloat();
+	specular.z = (*it).asFloat();
+
+	col = root["ambient"];
+	it = col.begin();
+	ambient.x = (*it++).asFloat();
+	ambient.y = (*it++).asFloat();
+	ambient.z = (*it).asFloat();
+
+	col = root["diffuse"];
+	it = col.begin();
+	diffuse.x = (*it++).asFloat();
+	diffuse.y = (*it++).asFloat();
+	diffuse.z = (*it).asFloat();
+
 }
