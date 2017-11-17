0b050a2ce0510cbc72506d503ec78a24b6e00120
CapitanLiteral <anaraguen@gmail.com>
2017-11-17 17:32:18 +0100

    Generate Buffers for mesh

--- Yog-Sothoth/Yog-Sothoth/Engine/Resources/R_Mesh.cpp
+++ Yog-Sothoth/Yog-Sothoth/Engine/Resources/R_Mesh.cpp
@@ -139,6 +139,8 @@ void R_Mesh::LoadMeshFile(const char* filename)
 
 	bytes = sizeof(float) * uv.numUV * 2;
 	memcpy(uv.uv, cursor, bytes);
+
+	GenBuffers();
 }
 
 void R_Mesh::Load(const aiMesh* aiMesh)
@@ -213,3 +215,47 @@ void R_Mesh::AddMeta(std::string prefab)
 void R_Mesh::RemoveMeta(std::string prefab)
 {
 }
+
+void R_Mesh::GenBuffers()
+{
+	//Generating GL Buffers
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, (GLuint*) &(vertices.idVertices));
+	glGenBuffers(1, (GLuint*) &(indices.idIndices));
+
+	glBindVertexArray(VAO);
+
+	//vertices	
+	glBindBuffer(GL_ARRAY_BUFFER, vertices.idVertices);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.numVertices * 3, vertices.vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	//indices	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices.idIndices);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * indices.numIndices, indices.indices, GL_STATIC_DRAW);
+
+	//normals
+	if (normals.normals != nullptr)
+	{
+		glGenBuffers(1, (GLuint*) &(normals.idNormals));
+		glBindBuffer(GL_ARRAY_BUFFER, normals.idNormals);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*normals.numNormals * 3, normals.normals, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+	}
+	//UV
+	if (uv.uv != nullptr)
+	{
+		glGenBuffers(1, (GLuint*) &(uv.idUV));
+		glBindBuffer(GL_ARRAY_BUFFER, uv.idUV);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.numVertices * 2, uv.uv, GL_STATIC_DRAW);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(2);
+	}
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+}
+
